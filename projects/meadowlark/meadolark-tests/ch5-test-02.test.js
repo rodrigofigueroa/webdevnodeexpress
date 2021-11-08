4d24a878aad3ba2b9bb5a1e7143e7f9c23c34671
shadowbeat274 <olivertate274@gmail.com>
2021-11-08 08:32:28 -0600

    changing file to test file and installling packages and runing test

--- projects/meadowlark/meadolark-tests/ch5-test-02.test.js
+++ projects/meadowlark/meadolark-tests/ch5-test-02.test.js
@@ -29,11 +29,11 @@
 
 const puppeteer   = require( 'puppeteer' )
 const portfinder  = require( 'portfinder' )
-const app         = require( './../ch5_02_meadowlark' )
+const app         = require( '../ch5_02_meadowlark' )
 let servidor  = null,
     port      = null
 
-beforeEach( async () => {
+;beforeEach( async () => {
   port = await portfinder.getPortPromise()
   servidor = app.listen( port )
 })
@@ -47,9 +47,9 @@ test( 'testing click and url about correctely', async () => {
   const page    = await browser.newPage()
   ; await page.goto( `http://127.0.0.1:${ port }` )
   ; await Promise.all([
-    page.waitForNavitation(),
+    page.waitForNavigation(),
     page.click( '[ data-test="about" ]' )
   ])
-  ;expect( page.url() ).toBe( `http://127.0.0.1:${ port }` )
+  ;expect( page.url() ).toBe( `http://127.0.0.1:${ port }/about` )
   ; await browser.close()
 })
