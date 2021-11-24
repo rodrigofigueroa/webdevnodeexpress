5447cec96619e2bec91e7789f15419d2b2602387
shadowbeat274 <olivertate274@gmail.com>
2021-11-23 18:21:38 -0600

    creating a send data and response and redirect and robust catch data

---
+++ projects/meadowlark/ch06_ch10_forms.js
@@ -0,0 +1,57 @@
+const express = require( 'express' ),
+      app     = express(),
+      hand    = require( 'express-handlebars' ),
+      port    = process.env.PORT || 3000,
+      bp      = require( 'body-parser' )
+
+//  Security
+app.disable( 'x-powered-by' )
+// Adding engine handlebars
+app.engine( 'handlebars', hand({
+  defaultLayout: 'main'
+}))
+app.set( 'view engine', 'handlebars' )
+// Body parser urlEncoded
+app.use( bp.urlencoded({ extended: false }))
+// Request Pages
+app.get( '/', ( req, res ) => res.render( 'home' ) )
+app.get( '/form', ( req, res ) => res.render( 'form' ) )
+app.get( '/thank-you', ( req, res ) => res.status( 303 ).render( 'thank-you') )
+// Reques Post
+app.post( '/form_data', ( req, res ) => {
+  console.log( req.body, req.xhr )
+  res.redirect( 303, 'thank-you' )
+})
+
+app.post( '/form_data_robust', ( req, res ) => {
+  try{
+    if( req.body.simulateError ){
+      throw new Error( 'Error' )
+    }
+    console.log(
+     `Contact: ${ req.body.name } email: ${ req.body.email }` 
+    )
+    res.format({
+      'text/html': () => res.redirect( 303, 'thank-you' ),
+      'application/json': () => res.status( 500 ).json({
+        success: true
+      })
+    })
+  }catch( err ){
+    console.log( `Error processing this ${ req.body.name } ${ req.body.email }` );
+    res.format({
+      'text/html': () => res.redirect( 303, 'thank-you' ),
+      'application/json': () => res.status( 500 ).json({
+        error: 'Error saving Content'
+      })
+    })
+  }
+})
+// Error and Not Found
+app.use( ( req, res ) => res.status( 404 ).render( '404' ) )
+
+app.use( ( err, req, res, next ) => req.status( 500 ).render( '500' ) )
+
+// Listen
+
+app.listen( port, console.log( `http://127.0.0.1:${ port }`))
