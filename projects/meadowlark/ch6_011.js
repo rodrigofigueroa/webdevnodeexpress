ebab267fbf563e6892eb42f8d42582961591f5d0
shadowbeat274 <olivertate274@gmail.com>
2021-11-28 17:43:00 -0600

    Makin experiments with put and using multer to check it out

--- projects/meadowlark/ch6_011.js
+++ projects/meadowlark/ch6_011.js
@@ -3,7 +3,9 @@ const express     = require( 'express' ),
       hand        = require( 'express-handlebars' ),
       bp          = require( 'body-parser' )
       port        = process.env.PORT || 1024,
-      { resolve } = require( 'path' )
+      { resolve } = require( 'path' ),
+      multer      = require( 'multer' ),
+      upload      = multer()
 
 // Variables
 const tours = [
@@ -20,8 +22,11 @@ const tours = [
 ];
 // Security
 app.disable( 'x-powered-by' )
+
 // body parser
-app.use( bp.urlencoded({ extended: false }))
+app.use( bp.json() ) 
+app.use( bp.urlencoded() )
+app.use( bp.urlencoded({ extended: true })) 
 
 // Engine
 app.engine( 'handlebars', hand({
@@ -59,5 +64,24 @@ app.post( '/normalform', ( req, res ) => {
     success: true
   })
 })
+// Update PUT
+const Users = [
+  { id: 0, userName: 'AnnaSophia Robb', age: 27 },
+  { id: 1, userName: 'Jesse Aarons', age: 27 }
+]
+// To use the link you need to use this method /api/user/1 
+app.put( '/api/user/:id', upload.none() ,( req, res ) => {
+  console.log( `req,params`, req.params, 'req.query', req.query, 'req.body', req.body )
+  const find = Users.find( itm => ( 
+    itm.id === parseInt( req.params.id ) 
+  ) ? itm : '' )
+  if( !find ) return res.status( 404 ).json({ 
+    success: false, error: 'No suchas user in the database'
+  })
+
+  console.log( find )
+  res.json({ success: true })
+})
+
 // Listen
 app.listen( port, console.log( `http://127.0.0.1:${ port }` ) )
